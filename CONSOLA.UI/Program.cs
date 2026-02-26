d5a89e9b7a2842f2164d9d827e3b3d3799c8c5a6
Fernando Des <ferbenor@gmail.com>
2026-02-26 01:39:06 -0500

    cambio automatico Debug/Release entre mock y core

--- CONSOLA.UI/Program.cs
+++ CONSOLA.UI/Program.cs
@@ -1,6 +1,8 @@
 using Velopack;
 using CONSOLA.Contratos;
+#if DEBUG
 using CONSOLA.MockDatos;
+#endif
 
 namespace CONSOLA
 {
@@ -16,12 +18,13 @@ namespace CONSOLA
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            // Para desarrollo (contratado): datos ficticios, sin conexion a Informix
+#if DEBUG
+            // Debug: datos ficticios - el contratado siempre trabaja asi
             IBaseDatosServicio servicio = new BaseDatosServicioMock();
-
-            // Para produccion: descomentar la linea de abajo y comentar la de arriba
-            // Requiere agregar referencia a CONSOLA.Core en el .csproj
-            // IBaseDatosServicio servicio = new CONSOLA.Core.BaseDatosServicio();
+#else
+            // Release: conexion real a Informix - solo para produccion
+            IBaseDatosServicio servicio = new CONSOLA.Core.BaseDatosServicio();
+#endif
 
             Application.Run(new FormLogin(servicio));
         }
